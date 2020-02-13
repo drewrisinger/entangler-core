33f6a78a07e050451ee28d7d760955940bde5001
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-02-13 18:15:36 -0500

    example (basic_experiment): refactor & clarify
    Remove unused print/rtio_log statements, and make easier to read/
    copy for users' own implementation.

--- example/basic_experiment.py
+++ example/basic_experiment.py
@@ -7,6 +7,7 @@ import artiq.language.units as aq_units
 import numpy
 import pkg_resources
 from artiq.language.core import kernel, delay, delay_mu, parallel
+from artiq.language.types import TInt32
 from artiq.coredevice.rtio import rtio_output
 from dynaconf import LazySettings
 
@@ -40,72 +41,90 @@ class EntanglerDemo(artiq_env.EnvExperiment):
         Note that this can be used in loopback mode. If you connect an output to
         one of the end outputs and observe a different output on an oscilloscope,
         you can see the entanglement end early when it detects an "event".
-        However, when the cable is unplugged it will run for the entire duration.
+        However, when the loopback cable is unplugged it will run for the full duration.
         """
-
-        # NOTE: following break_realtimes/delays are for rtio_log statements, should not be used in production
-        # *** Setup ***
         self.core.reset()
         self.core.break_realtime()
-        self.entangler.init()   # disable Entangler outputs, write master
-        self.out0_0.pulse(1.5 * aq_units.us)
-        rtio_log("entangler", "setting inputs")
-        delay(15 * aq_units.us)
-        # setup inputs & set edge sensitivity (0b01 for rising edge sensitivity, 0b10 for falling, 0b11=both)
-        for ttl_inout in self.inputs:
-            ttl_inout.input()
-            # delay_mu(1)
-            # ttl_inout._set_sensitivity(1)
-        rtio_log("entangler", "start demo")
-        # self.core.break_realtime()
-        for channel in range(num_outputs):
-            # delay(1 * aq_units.us)
-            self.entangler.set_timing_mu(channel, 100, 1000)
-        for channel in range(num_inputs):
-            # input window start must be at least 8mu
-            self.entangler.set_timing_mu(channel + num_outputs, 10, 1000)
-        # delay(1 * aq_units.us)
-        # NOTE: must set enable, defaults to disabled. If not standalone, tries to sync w/ slave (which isn't there) & doesn't start
-        self.entangler.set_config(enable=True, standalone=True)
-        # delay(1 * aq_units.us)
-        self.entangler.set_cycle_length_mu(1200)
-        # delay(1 * aq_units.us)
-        self.entangler.set_patterns([0b1111, 0b1000, 0b0011])
-        # delay(1 * aq_units.us)
-        rtio_log("entangler", "Ended setup")
-        delay(10 * aq_units.us)
-        # self.core.break_realtime()
-        status = self.entangler.get_status()
-        # self.core.break_realtime()
-        rtio_log("entangler", "Status:", status)
-        # self.core.break_realtime()
+        self.init()
+        self.setup_entangler(
+            cycle_len=1200,
+            out_start=100,
+            out_stop=1000,
+            in_start=10,
+            in_stop=1000,
+            pattern_list=[0b1111, 0b1000, 0b0011],
+        )
+        end_timestamp, reason = self.run_entangler(10000)
+        self.check_entangler_status()
 
-        # # *** Run ***
-        delay(60 * aq_units.us)
-        runtime_mu = numpy.int32(10000)
+        print("entangler", "Finished", reason)
+
+    @kernel
+    def init(self):
+        """One-time setup on device != entangler."""
+        self.out0_0.pulse(1.5 * aq_units.us)  # marker signal for observing timing
+        for ttl_input in self.inputs:
+            ttl_input.input()
+
+    @kernel
+    def setup_entangler(
+        self, cycle_len, out_start, out_stop, in_start, in_stop, pattern_list
+    ):
+        """Configure the entangler.
+
+        These mostly shouldn't need to be changed between entangler runs, though
+        you can with most of the set commands.
+
+        Args:
+            cycle_len (int): Length of each entanglement cycle.
+            out_start (int): Time in cycle when all outputs should turn on.
+            out_stop (int): Time in cycle when all outputs should turn off (deassert)
+            in_start (int): Time in cycle when all inputs should start looking for
+                input signals
+            in_stop (int): Time in cycle when all inputs should STOP looking for
+                input signals.
+            pattern_list (list(int)): List of patterns that inputs are matched
+                against. Matching ANY will stop the entangler.
+        """
+        self.entangler.init()
+        for channel in range(num_outputs):
+            self.entangler.set_timing_mu(channel, out_start, out_stop)
+        for channel in range(num_inputs):
+            self.entangler.set_timing_mu(channel + num_outputs, in_start, in_stop)
+
+        # NOTE: must set enable, defaults to disabled. If not standalone, tries to sync
+        # w/ slave (which isn't there) & doesn't start
+        self.entangler.set_config(enable=True, standalone=True)
+        self.entangler.set_cycle_length_mu(cycle_len)
+        self.entangler.set_patterns(pattern_list)
+
+    @kernel
+    def run_entangler(self, timeout_length: TInt32):
+        """Run the entangler for a max time and wait for it to succeed/timeout."""
         with parallel:
-            # ONE OF FOLLOWING IS REQUIRED
             # This generates output events on the bus -> entangler
             # when rising edges are detected
-            self.inputs[0].gate_rising_mu(numpy.int64(runtime_mu))
-            self.inputs[1].gate_rising_mu(numpy.int64(runtime_mu))
-            self.inputs[2].gate_rising_mu(numpy.int64(runtime_mu))
-            self.inputs[3].gate_rising_mu(numpy.int64(runtime_mu))
-            # rtio_output(self.inputs[0].target_sample, 1)
-            # rtio_output(self.inputs[3].target_sample, 1)
-            end_timestamp, reason = self.entangler.run_mu(runtime_mu)
-        self.core.break_realtime()  # must wait after entangler ends to schedule new events
+            self.inputs[0].gate_rising_mu(numpy.int64(timeout_length))
+            self.inputs[1].gate_rising_mu(numpy.int64(timeout_length))
+            self.inputs[2].gate_rising_mu(numpy.int64(timeout_length))
+            self.inputs[3].gate_rising_mu(numpy.int64(timeout_length))
+            end_timestamp, reason = self.entangler.run_mu(timeout_length)
+        # must wait after entangler ends to schedule new events.
+        # Doesn't strictly NEED to break_realtime, but it's safe.
+        self.core.break_realtime()
+        # Disable entangler control of outputs
         self.entangler.set_config(enable=False)
-        if reason != 0x3FFF:
-            rtio_log("entangler", "run timestamp:", end_timestamp)
-        else:
-            rtio_log("entangler", "timeout@", end_timestamp)
-        delay(100 * aq_units.us)
-        # self.core.break_realtime()
-        rtio_log("entangler", "end reason:", reason)
-        # self.core.break_realtime()
 
-        # Check status, not required
+        # You might also want to disable gating for inputs, but out-of-scope
+
+        return end_timestamp, reason
+
+    @kernel
+    def check_entangler_status(self):
+        """Get Entangler end status and log to coreanalyzer.
+
+        Not required in normal usage, recognized pattern is returned by run_entangler().
+        """
         delay(100 * aq_units.us)
         status = self.entangler.get_status()
         if status & 0b010:
@@ -113,24 +132,14 @@ class EntanglerDemo(artiq_env.EnvExperiment):
         else:
             rtio_log("entangler", "End status:", status)
 
-        # self.core.break_realtime()
         delay(100 * aq_units.us)
         num_cycles = self.entangler.get_ncycles()
         rtio_log("entangler", "#cycles:", num_cycles)
-        # self.core.break_realtime()
         delay(100 * aq_units.us)
         ntriggers = self.entangler.get_ntriggers()
         rtio_log("entangler", "#triggers (0 if no ref)", ntriggers)
         for channel in range(num_inputs):
-            # self.core.break_realtime()
             delay(150 * aq_units.us)
             channel_timestamp = self.entangler.get_timestamp_mu(channel)
-            rtio_log("entangler",
-                "Ch",
-                channel,
-                ": ts=",
-                channel_timestamp,
-            )
-        delay(100 * aq_units.us)
-        self.out0_0.pulse(5 * aq_units.us)
-        print("entangler", "Finished", status)
+            rtio_log("entangler", "Ch", channel, ": ts=", channel_timestamp)
+        delay(150 * aq_units.us)
