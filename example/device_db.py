138301a39e86baee3130a17a158e691c7202b969
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-02-13 18:11:54 -0500

    example: files with running_output
    Examples work well with or without running_output.
    Note that running_output must be enabled both in settings, JSON, and
    device_db.py in order to work. Not ideal, but not high priority for now.

--- example/device_db.py
+++ example/device_db.py
@@ -1,5 +1,7 @@
 """Minimal ARTIQ Device DB for demonstrating the Entangler."""
 
+using_running_output = False
+
 device_db = {
     "core": {
         "type": "local",
@@ -11,11 +13,38 @@ device_db = {
         "type": "local",
         "module": "entangler.driver",
         "class": "Entangler",
-        "arguments": {"channel": 17, "is_master": True},
+        "arguments": {
+            "channel": 15 if using_running_output else 16,
+            "is_master": True,
+        },
         "comments": [
             "Change the channel to match console when building gateware. "
             "Run 'python -m entangler.kasli_generic entangler_gateware_example.json "
-            "--no-compile-software --no-compile-gateware'",
-        ]
+            "--no-compile-software --no-compile-gateware'. "
+            "If you use a running_output (in JSON), you must decrement this channel."
+        ],
     },
 }
+for i in range(16):
+    if i < 12:
+        if i == 11 and using_running_output:
+            # skip this channel
+            continue
+        device_db["out{}-{}".format(i // 8, i % 8)] = {
+            "type": "local",
+            "module": "artiq.coredevice.ttl",
+            "class": "TTLOut",
+            "arguments": {"channel": i},
+        }
+    else:
+        if using_running_output:
+            # -1 to skip running_output channel
+            channel = i - 1
+        else:
+            channel = i
+        device_db["in{}-{}".format(i // 8, i % 8)] = {
+            "type": "local",
+            "module": "artiq.coredevice.ttl",
+            "class": "TTLInOut",
+            "arguments": {"channel": channel},
+        }
